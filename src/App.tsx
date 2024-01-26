f4a3f557ffbd4a0cfe9d090078f7a907c59da492
Matheus Paice <matheus.paice@jbtec.com.br>
2024-01-26 10:10:02 -0300

    fix: correct bug render page

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <>
+    <div>
       <RouterProvider router={router} />
       <ToastContainer />
-    </>
+    </div>
   );
 }
 
